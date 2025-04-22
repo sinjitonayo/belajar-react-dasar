845510e87ce0e50c8af97e3da156033360749393
sinjitonayo <tanakashinji23@gmail.com>
2025-04-22 18:33:23 +0800

    feat: enhance AlertButton to log click events with console.info

--- src/button/AlertButton.jsx
+++ src/button/AlertButton.jsx
@@ -1,5 +1,6 @@
 export default function AlertButton({ text, message }) {
-  function handleClick() {
+  function handleClick(e) {
+    console.info("Button clicked:", e);
     alert(message);
   }
   return <button onClick={handleClick}>{text}</button>;
